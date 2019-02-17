8724c1b3c9a130bf63c37c095fc9ca7ecd368420
ellie <arp6333@g.rit.edu>
2019-02-16 23:52:26 -0500

    final.html

--- templates/assets/js/init.js
+++ templates/assets/js/init.js
@@ -1,16 +1,27 @@
 // initialize necessary materialize components
 (function($){
     $(function(){
+        // side bar navigation (start.html)
         $('.sidenav').sidenav();
         
-        $('.modal').modal({
+        // modal (2 is from guess.html, 1 is from start.html)
+         $('#modal2').modal({
+            dismissible: true
+        });
+        $('#modal1').modal({
             dismissible: false
         });
-            
+        
+        // card tab (final.html)
+        $('.tabs').tabs();
+        
+        // dropdown (settings in start.html)
         $('.dropdown-trigger').dropdown();
         
+        // i think this is from the login in form 
         $('select').formSelect();
         
+        // text input character count (login.html)
         $('input#input_text, textarea#textarea2').characterCounter();
     });
 })(jQuery);
